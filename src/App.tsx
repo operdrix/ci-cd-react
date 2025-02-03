f3098cff94d6071f6ef936834e18e9461c1c2fe6
s-kenza <kenza.schuler@gmail.com>
2025-02-03 19:45:46 +0100

    test: add error on purpose

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        <button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
